080e7c14c43036be51a31e6711031410b975ee2f
Taha <HILALBIK_TAHA@emsi-edu.ma>
2024-08-18 18:15:17 +0100

    Commit 4

--- src/main/java/org/xproce/pfaram/controller/RapportController.java
+++ src/main/java/org/xproce/pfaram/controller/RapportController.java
@@ -3,9 +3,13 @@ package org.xproce.pfaram.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.xproce.pfaram.entity.Rapport;
 import org.xproce.pfaram.service.RapportService;
+
+import java.util.List;
 
 @Controller
 public class RapportController {
@@ -13,29 +17,30 @@ public class RapportController {
     @Autowired
     private RapportService rapportService;
 
-    @GetMapping("/listRapport")
-    public String listRapport() {
-        return "listRapport";
-    }
-    @GetMapping("/rapports")
-    public String rapports() {
-        return "listRapport";
-    }
-
     @GetMapping("/mesRapports")
-    public String mesRapports() {
+    public String mesRapports(Model model) {
+        List<Rapport> rapports = rapportService.findAllRapports();
+        model.addAttribute("rapports", rapports);
         return "mesRapports";
     }
 
     @GetMapping("/rapports/new")
     public String createRapportForm(Model model) {
         model.addAttribute("rapport", new Rapport());
-        return "create_rapport"; // Nom du template Thymeleaf
+        return "create_rapport";
     }
 
-    @PostMapping("/mesRapports")
-    public String saveRapport(@ModelAttribute("rapport") Rapport rapport) {
+    @PostMapping("/rapports")
+    public String saveRapport(Rapport rapport) {
         rapportService.saveRapport(rapport);
-        return "redirect:/mesRapports"; // Redirect after saving
+        return "redirect:/mesRapports";
+    }
+
+    // Méthode pour afficher les détails d'un rapport
+    @GetMapping("/rapports/{id}")
+    public String detailRapport(@PathVariable Long id, Model model) {
+        Rapport rapport = rapportService.findRapportById(id);
+        model.addAttribute("rapport", rapport);
+        return "detail_rapport";
     }
 }
